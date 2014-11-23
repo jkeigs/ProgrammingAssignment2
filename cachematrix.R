65cc8d92f75b281a3dec406be821c641c299ef93
jkeigs <jacobkeigan@Jacobs-MBP.home>
2014-11-23 08:49:55 -0800

    Final 2
    Modified after running unit test code from here:
    https://class.coursera.org/rprog-009/forum/thread?thread_id=701

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,6 @@
 #' https://class.coursera.org/rprog-009/forum/thread?thread_id=457
 
 makeCacheMatrix <- function(x) {      # input x will be a matrix
-  print(x)
   m <- NULL    #  m will be our 'inverse' and it's reset to NULL every 
   #    time makeVector is called
   
@@ -21,24 +20,25 @@ makeCacheMatrix <- function(x) {      # input x will be a matrix
     m <<- NULL      # resets the inverse to NULL, basically what happens when a new object is generated.
   }
   
-  setSolve <- function(solve)  { 
+  setinverse <- function(solve)  { 
     m <<- solve 
   }
   # this is called by cacheSolve() during the first cacheSolve()
   #  access and it will store the value using superassignment
   
-  getSolve <- function() { m } # this will return the cached value to cacheSolve() on
+  getinverse <- function() { m } # this will return the cached value to cacheSolve() on
   #  subsequent accesses
   
   list(get = get,          #  OK, this is accessed each time makeCacheMatrix() is called,       
-       setSolve = setSolve,  #   that is, each time we make a new object.  This is a list of 
-       getSolve = getSolve)  #   the internal functions ('methods') so a calling function
+       set = set,
+       setinverse = setinverse,  #   that is, each time we make a new object.  This is a list of 
+       getinverse = getinverse)  #   the internal functions ('methods') so a calling function
   #   knows how to access those methods.      
   
 }
 
 cacheSolve <- function(x, ...) {   # the input x is an object created by makeCacheMatrix
-  m <- x$getSolve()               # accesses the object 'x' and gets the inverted matrix
+  m <- x$getinverse()               # accesses the object 'x' and gets the inverted matrix
   if(!is.null(m)) {              # if inverse was already cached (not NULL) ...
     
     message("getting cached data")  # ... send this message to the console
@@ -47,13 +47,26 @@ cacheSolve <- function(x, ...) {   # the input x is an object created by makeCac
   }
   data <- x$get()        # we reach this code only if x$getSolve() returned NULL
   m <- solve(data, ...)   # if m was NULL then we have to calculate the inverse
-  x$setSolve(m)           # store the calculated inverse value in x (see setSolve() in makeVector
+  x$setinverse(m)           # store the calculated inverse value in x (see setSolve() in makeVector
   m               # return the mean to the code that called this function
 }
 
-set.seed(19)
-mx <- matrix(rnorm(4), nrow = 2,ncol = 2)
-bM <- makeCacheMatrix(mx) 
-cacheSolve(bM) 
-cacheSolve(bM) 
-solve(mx)
+## Testing
+# set.seed(19)
+# mx <- matrix(rnorm(4), nrow = 2,ncol = 2)
+# bM <- makeCacheMatrix(mx) 
+# cacheSolve(bM) 
+# cacheSolve(bM) 
+# solve(mx)
+# 
+# A <- matrix(c(2,4,3,1),nrow=2,ncol=2,byrow=TRUE)
+# B <- makeCacheMatrix(A)
+# B$get()
+# B$getinverse()
+# cacheSolve(B)
+# cacheSolve(B)
+# B$getinverse()
+# B$set(matrix(c(1,2,3,4),nrow=2,ncol=2,byrow=TRUE))
+# B$get()
+# B$getinverse()
+# cacheSolve(B)
